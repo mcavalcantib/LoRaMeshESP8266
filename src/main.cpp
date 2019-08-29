94437fe4b6bdf0a5189d35528ec7639dd21cd108
Moises Cavalcanti <moisesc.bezerril@gmail.com>
2019-08-29 00:09:15 -0300

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,55 @@ uint16_t AdcIn[2];
 /* SoftwareSerial handles */
 SoftwareSerial* hSerialCommands = NULL;
 
+
+MeshStatus_Typedef WriteConfig(uint16_t id, uint16_t net, uint32_t uniqueId) {
+    uint8_t crc = 0;
+    uint8_t bufferPayload[31];
+    uint8_t payloadSize;
+    uint8_t i = 0;
+    uint8_t command;
+    uint16_t id = 0;
+
+    /* Asserts parameters */
+    if (net == NULL) return MESH_ERROR;
+    if (uniqueId == 0) return MESH_ERROR;
+    if (hSerialCommands == NULL) return MESH_ERROR;
+
+    /* Loads dummy bytes */
+    for (i = 0; i < 3; i++) bufferPayload[i] = 0x00;
+
+    PrepareFrameCommand(idIn, CMD_REMOTEREAD, &bufferPayload[0], i);
+
+    /* Sends packet */
+    SendPacket();
+
+    /* Flush serial input buffer */
+    SerialFlush(hSerialCommand);
+
+    /* Waits for response */
+    if (ReceivePacketCommand(&id, &command, &bufferPayload[0], &payloadSize,
+                             5000) != MESH_OK)
+        return MESH_ERROR;
+
+    /* Checks command */
+    if ((command != CMD_REMOTEREAD) && (command != CMD_LOCALREAD))
+        return MESH_ERROR;
+
+    /* Stores the received data */
+    if (idOut != NULL) /* Local read */
+    {
+        deviceId = id;
+        *idOut = id;
+    }
+    *net = (uint16_t)bufferPayload[0] | ((uint16_t)(bufferPayload[1]) << 8);
+    *uniqueId = (uint32_t)bufferPayload[2] |
+                ((uint32_t)(bufferPayload[3]) << 8) |
+                ((uint32_t)(bufferPayload[4]) << 16) |
+                ((uint32_t)(bufferPayload[5]) << 24);
+
+    return MESH_OK;
+}
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(16, OUTPUT);
@@ -52,7 +101,19 @@ void setup() {
     Serial.println(localUniqueId, HEX);
     Serial.print("\n");
   }
-  
+  Serial.println("Sending ID and NET Configurations......");
+  if(LocalRead(&localId, &localNet, &localUniqueId) != MESH_OK)
+    Serial.print("Couldn't read local ID\n\n");
+  else
+  {
+    Serial.print("Local ID: ");
+    Serial.println(localId);
+    Serial.print("Local NET: ");
+    Serial.println(localNet);
+    Serial.print("Local Unique ID: ");
+    Serial.println(localUniqueId, HEX);
+    Serial.print("\n");
+  }
   delay(500);
 }
 
